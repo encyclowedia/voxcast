28e9ee82d1b5b3484c25b2d3b4a5667d9fe7fa32
rohit <rohit@rohit-aw-android>
2014-10-08 19:12:36 +0530

    Commiting the project for the first time...

---
+++ Voxcast/src/com/voxcast/MyFragment1.java
@@ -0,0 +1,27 @@
+package com.voxcast;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class MyFragment1 extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+		Button button = new Button(getActivity());
+		button.setText("2");
+		return button;
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+	}
+
+}
