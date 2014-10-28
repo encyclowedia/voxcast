e3e45689d7366e8be66d87f7b9c2964caa38a78a
rohit <rohit@rohit-aw-android>
2014-10-28 19:49:29 +0530

    Commiting the Changes

--- Voxcast/src/com/voxcast/constant/Constant.java
+++ Voxcast/src/com/voxcast/constant/Constant.java
@@ -5,9 +5,8 @@ public class Constant {
 	public static final int SPLASH_DISPLAY_TIME = 3000; // in 5 mili second
 	public static final String MY_PREF_NAME = "My_Pref";
 	public static final String MY_PREF_SPLASH_KEY = "Splash";
-	public static final int RESULT_GALLERY_MULTIPLEIMAGE = 0;
-	public static final int RESULT_GALLERY_VIDEOIMAGE = 1;
-	public static final int RESULT_GALLERY_CAMERA = 2;
-	public static final int CAMERA_REQUEST = 3;
+	public static final int RESULT_GALLERY_VIDEO_IMAGE = 0;
+	public static final int RESULT_GALLERY_VIDEOCAPTURE = 1;
+	public static final int RESULT_GALLERY_CAMERA_IMAGE = 2;
 
 }
