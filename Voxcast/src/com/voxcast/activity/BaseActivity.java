2e0bba76dcda64a0f6ce0d2d0e202656689c8416
root <root@mobile-aw-prakhar>
2014-11-17 12:31:58 +0530

    Add model /get/post

--- Voxcast/src/com/voxcast/activity/BaseActivity.java
+++ Voxcast/src/com/voxcast/activity/BaseActivity.java
@@ -20,7 +20,7 @@ import android.widget.ListView;
 
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.assist.PauseOnScrollListener;
+import com.nostra13.universalimageloader.core.listener.PauseOnScrollListener;
 import com.voxcast.R;
 import com.voxcast.listeners.OnScrollListener;
 
